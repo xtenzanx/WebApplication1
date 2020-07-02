f4df044cc2ded8631620efc195a8f87c91c349e3
xtenzanx <xtenzanxt1@gmail.com>
2020-07-02 16:37:34 +0800

    官方MVC範例完成, 附加複製與排序功能

---
+++ Migrations/20200701062753_Rating.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication1.Migrations
+{
+    public partial class Rating : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Movie",
+                type: "decimal(18, 2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Rating",
+                table: "Movie",
+                nullable: true);
+
+            migrationBuilder.AddColumn<short>(
+                name: "Star",
+                table: "Movie",
+                type: "smallint",
+                nullable: false,
+                defaultValue: (short)0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Movie");
+
+            migrationBuilder.DropColumn(
+                name: "Star",
+                table: "Movie");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Movie",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18, 2)");
+        }
+    }
+}
